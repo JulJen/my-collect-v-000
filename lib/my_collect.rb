8e42fce3b38e1b98c1e329061c3915d1def954cf
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-17 18:08:22 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
     i = 0
     empty_array = []
     while i < collection.length
-      empty_array << yield(collection[i]).upcase
+      empty_array << yield(collection[i])
       i += 1
     end
     empty_array
